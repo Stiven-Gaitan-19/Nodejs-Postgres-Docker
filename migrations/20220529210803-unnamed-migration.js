f7e3619284d74a5b3e8b76841595d7e303f3559b
Stiven Gaitan <johan.gaitan@mqa-bc.com>
2022-06-16 22:01:55 -0500

    first schema was terminated also first migration

--- migrations/20220529210803-unnamed-migration.js
+++ migrations/20220529210803-unnamed-migration.js
@@ -1,13 +1,29 @@
 'use strict';
 
 let { userSchema } = require('../models/user');
+let { customerSchema } = require('../models/customer');
+let { categorySchema } = require('../models/category');
+let { productSchema } = require('../models/product');
+let { orderSchema } = require('../models/order');
+let { orderProductSchema } = require('../models/order-product');
 
 module.exports = {
   async up(queryInterface) {
-    queryInterface.createTable('users', userSchema);
+
+    await queryInterface.createTable('users', userSchema);
+    await queryInterface.createTable('customers', customerSchema);
+    await queryInterface.createTable('categories', categorySchema);
+    await queryInterface.createTable('products', productSchema);
+    await queryInterface.createTable('orders', orderSchema);
+    await queryInterface.createTable('orders_products', orderProductSchema);
   },
 
   async down(queryInterface) {
-    queryInterface.dropTable('users');
+    await queryInterface.dropTable('users');
+    await queryInterface.dropTable('customers');
+    await queryInterface.dropTable('categories');
+    await queryInterface.dropTable('products');
+    await queryInterface.dropTable('orders');
+    await queryInterface.dropTable('orders_products')
   },
 };
