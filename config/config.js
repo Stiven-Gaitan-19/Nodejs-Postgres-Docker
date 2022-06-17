89fda76cc601a844870707d9924f4582dded6380
Stiven Gaitan <johan.gaitan@mqa-bc.com>
2022-06-16 21:43:56 -0500

    db ssl config for migrations was fixed

--- config/config.js
+++ config/config.js
@@ -1,15 +1,17 @@
 require('dotenv').config();
 
 module.exports = {
-  development: {
-    url: process.env.DATABASE_URL,
-    dialect: 'postgres',
-  },
-  production: {
-    url: process.env.DATABASE_URL,
-    dialect: 'postgres',
-    ssl:{
-      rejectUnauthorized: false
-    }
-  },
+	development: {
+		url: process.env.DATABASE_URL,
+		dialect: 'postgres',
+	},
+	production: {
+		url: process.env.DATABASE_URL,
+		dialect: 'postgres',
+		dialectOptions: {
+			ssl: {
+				rejectUnauthorized: false,
+			},
+		},
+	},
 };
