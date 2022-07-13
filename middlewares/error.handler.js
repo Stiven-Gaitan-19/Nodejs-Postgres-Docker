5c195f24ad786f52fd8b3d73046174c003d4bfc0
Stiven Gaitan <johan.gaitan@mqa-bc.com>
2022-07-12 20:11:01 -0500

    error middleware was changed to print the error

--- middlewares/error.handler.js
+++ middlewares/error.handler.js
@@ -1,22 +1,24 @@
-function logErrors (err, req, res, next) {
-  console.error(err);
-  next(err);
+const boom = require('@hapi/boom');
+
+function logErrors(err, req, res, next) {
+	console.error(err);
+	next(err);
 }
 
-function errorHandler(err, req, res, next) {
-  res.status(500).json({
-    message: err.message,
-    stack: err.stack,
-  });
+function errorHandler(err, req, res, _next) {
+	res.status(500).json({
+		message: err.message,
+		stack: err.stack,
+	});
 }
 
-function boomErrorHandler(err, req, res, next) {
-  if (err.isBoom) {
-    const { output } = err;
-    res.status(output.statusCode).json(output.payload);
-  }
-  next(err);
+function boomErrorHandler(err, req, res, _next) {
+	if (!err.isBoom) {
+		console.error(err);
+		err = boom.internal();
+	}
+	const { output } = err;
+	res.status(output.statusCode).json(output.payload);
 }
 
-
-module.exports = { logErrors, errorHandler, boomErrorHandler }
+module.exports = { logErrors, errorHandler, boomErrorHandler };
